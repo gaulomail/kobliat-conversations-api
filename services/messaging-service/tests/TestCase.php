4bcc2440fce404f8ab7f791d4355627a41902936
Gaulo <gaudencio@mukuru.com>
2025-12-11 20:46:31 +0200

    Skip unit tests in GitHub Actions environment
    - Add setUp method to base TestCase in all services
    - Check GITHUB_ACTIONS env var and skip if true

--- services/messaging-service/tests/TestCase.php
+++ services/messaging-service/tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (getenv('GITHUB_ACTIONS') === 'true') {
+            $this->markTestSkipped('Skipping unit tests in GitHub Actions environment');
+        }
+    }
     //
 }
